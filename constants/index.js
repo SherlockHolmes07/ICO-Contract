f017780a32681bd3e46a5909c34553f3bbb89fed
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-11-24 22:44:14 +0530

    ICO Contract Completed

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xA205168067dAaCD75ab394f92Fc9FdF6a988ffee";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
